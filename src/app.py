0329c01cb6e92c7cf285462271228c2c9725586a
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-12-06 19:45:34 +0000

    starwars api

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planets
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,25 +28,95 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=["GET", "POST"])
+def handle_people():
+    if request.method == "POST":
+        name = request.json["name"]
+        description = request.json["description"]
+        new_person = People(
+            name=name,
+            description=description
+        )
 
-    return jsonify(response_body), 200
+        return jsonify(new_person.serialize()), 201
 
-# this only runs if `$ python src/app.py` is executed
+    people = People.query.all()
+    people_dictionaries = []
+    for person in people:
+        people_dictionaries.append(
+            person.serialize()
+        )
+
+    return jsonify(people_dictionaries), 200
+
+
+@app.route('/people/<int:people_id>', methods=["GET"])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if person is None:
+        return jsonify({
+            "msg": "Character not found"
+        }), 404
+    return jsonify(person.serialize()), 200
+
+
+@app.route('/favorite/people/<int:people_id>', methods=["POST"])
+def add_favorite_person(people_id):
+    user_id = request.json.get("user_id")
+
+    # 1. get the user
+    user = User.query.get(user_id)
+    if not user:
+        return {"error": "User not found"}, 404
+
+    # 2. get the person
+    person = People.query.get(people_id)
+    if not person:
+        return {"error": "Person not found"}, 404
+
+    # 3. add person to user's favorites using relationship
+    user.favorites.append(person)
+
+    # 4. commit
+    db.session.commit()
+
+    return {"message": "Favorite added successfully"}
+
+
+@app.route('/users', methods=["GET", "POST"])
+def handle_users():
+    if request.method == "POST":
+        email = request.json["email"]
+        new_user = User(
+            email=email,
+        )
+
+        return jsonify(new_user.serialize()), 201
+
+    users = User.query.all()
+    users_dictionaries = []
+    for user in users:
+        users_dictionaries.append(
+            user.serialize()
+        )
+
+    return jsonify(users_dictionaries), 200
+
+    # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,104 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    String, Column, Table, ForeignKey
+)
+from sqlalchemy.orm import (
+    DeclarativeBase, Mapped,
+    mapped_column, relationship,
+)
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class Base(DeclarativeBase):
+    pass
+
+
+favorite_table = Table(
+    "favorite_table",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+    Column("people_uid", ForeignKey("people.uid"), primary_key=True)
+)
+
+
+# class Favorites(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+#     person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    favorites: Mapped[list["People"]] = relationship(
+        "People",
+        back_populates="favorites",
+        secondary=favorite_table
+    )
+
+    def __init__(self, email):
+        self.email = email
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+
+class People (db.Model):
+    __tablename__ = "people"
+    uid: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+    favorites: Mapped[list["User"]] = relationship(
+        "User",
+        back_populates="favorites",
+        secondary=favorite_table
+    )
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
+
+    def serialize(self):
+        return {
+            "uid": self.uid,
+            "name": self.name,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planets (db.Model):
+    uid: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "uid": self.uid,
+            "name": self.name,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
